3091afbedbb04f5c9154b6600dc05f3d2c4f9805
lechuza2013 <julian_s_99@live.com>
2024-05-15 10:11:57 -0300

    refactor: css fix

--- src/components/movieCard.tsx
+++ src/components/movieCard.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function MovieCard({ episode_id, title, image, id}){
     // /${episode_id}
-    return (<div className="group items-center flex flex-col">
+    return (<div className="group items-center flex flex-col p1.5">
         <Link href={`/film/${id}`}> 
             <img 
             className='rounded-2xl sm:rounded-t-lg group-hover:opacity-75 transition-opacity duration-300' 
@@ -10,11 +10,9 @@ export default function MovieCard({ episode_id, title, image, id}){
             alt="Film poster" 
             height={200} 
             width={300}
-            style={{
-                margin: "20px"
-            }}
+            
             />
-            <h1 className='text-center text-2xl md:text-4xl lg:text-2xl font-medium'>{title}</h1>
+            <h1 className='text-center text-2xl md:text-3xl lg:text-2xl font-medium'>{title}</h1>
             <h3 className='text-center text-lg md:text-xl'>EPISODE <span className='text-amber-300'>{episode_id}</span></h3>
         </Link>
     </div>)
