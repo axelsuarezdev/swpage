ad51660812917a18ad1413ae818d1b316cb39543
lechuza2013 <julian_s_99@live.com>
2024-05-15 11:42:36 -0300

    refactor: css fix v3

--- src/components/movieCard.tsx
+++ src/components/movieCard.tsx
@@ -10,7 +10,7 @@ export default function MovieCard({ episode_id, title, image, id}){
             alt="Film poster" 
             height={200} 
             width={300}
-            
+            style={{margin: "0 auto"}}
             />
             <h1 className='text-center text-2xl md:text-3xl lg:text-2xl font-medium'>{title}</h1>
             <h3 className='text-center text-lg md:text-xl'>EPISODE <span className='text-amber-300'>{episode_id}</span></h3>
